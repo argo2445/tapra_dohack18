338c53e2579e1cdcd368baa763cc5367f919920d
argo2445 <argozwoviervierfuenf@gmail.com>
2018-07-21 17:07:05 +0200

    Added script to create a minimized datatable

---
+++ shrinkDB.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import psycopg2
+
+# SELECT date_trunc('day', date) AS "Day" ,stid,  AVG(e5), AVG(e10), AVG(diesel)
+# FROM gas_station_information_history
+# GROUP BY 1, stid
+# ORDER BY 1 
+
+conn=psycopg2.connect(dbname='tankerkoenig16', user='postgres', password='geheim', host='192.168.0.2', port='5432')
+reader=conn.cursor()
+writer=conn.cursor()
+
+reader.execute('SELECT date_trunc(\'day\', date) AS "Day" ,stid,  AVG(e5), AVG(e10), AVG(diesel) FROM gas_station_information_history GROUP BY 1, stid ORDER BY 1 ')
+
+while(True):
+  row=reader.fetchone()
+  if row==None:
+    break
+  writer.execute('INSERT INTO gas_station_min_history (stid, e5, e10, diesel, date) VALUES (%s, %s, %s, %s, %s);',(row[1], row[2], row[3], row[4], row[0]))
+  conn.commit()
