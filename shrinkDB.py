2e6bce2d1427cfddc01b2c56bf5bd8ca96e5811e
argo2445 <argozwoviervierfuenf@gmail.com>
2018-07-22 00:38:19 +0200

    Filter invalid Data

--- shrinkDB.py
+++ shrinkDB.py
@@ -10,13 +10,13 @@ conn=psycopg2.connect(dbname='tankerkoenig16', user='postgres', password='geheim
 reader=conn.cursor()
 writer=conn.cursor()
 
-reader.execute('SELECT date_trunc(\'day\', date) AS "Day" ,stid,  AVG(e5), AVG(e10), AVG(diesel) FROM gas_station_information_history GROUP BY 1, stid ORDER BY 1 ')
-
+reader.execute('SELECT date_trunc(\'day\', date) AS "Day" ,stid,  AVG(e5), AVG(e10), AVG(diesel) FROM gas_station_information_history WHERE date > now() - interval \'12 months\'  GROUP BY 1, stid ORDER BY 1 DESC')
+print("Abfrage abgeschlossen - beginne mit Verarbeitung")
 while(True):
   row=reader.fetchone()
   if row==None:
     break
   # Check for invalid values
-  row[2:] = [val if val>0 else None for val in row[2:]]
-  writer.execute('INSERT INTO gas_station_min_history (stid, e5, e10, diesel, date) VALUES (%s, %s, %s, %s, %s);',(row[1], row[2], row[3], row[4], row[0]))
+  #row[2:] = [val if val>0 else None for val in row[2:]]
+  writer.execute('INSERT INTO gas_station_min_history (stid, e5, e10, diesel, date) VALUES (%s, %s, %s, %s, %s);',(row[1], (row[2] if row[2]>0 else None), (row[3] if row[3]>0 else None), (row[4] if row[4]>0 else None), row[0]))
   conn.commit()
